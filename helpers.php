1e31c62a88b8b5a62ab61dd7e06abe49fdf2d765
Kevin Quail <kevinq.7365@gmail.com>
2024-02-18 11:03:23 -0600

    Initial commit for project setup

---
+++ helpers.php
@@ -0,0 +1,11 @@
+<!-- this will have helper functions for entire site -->
+<?php
+/**
+*
+* @param string path
+* @return string
+*/
+
+function basePath($path = '') {
+    return __DIR__ . '/' . $path;
+}
