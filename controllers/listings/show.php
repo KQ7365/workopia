c24d141d0f6a83b9131456412b3e39efe35bbd40
Kevin Quail <kevinq.7365@gmail.com>
2024-03-12 09:57:00 -0500

    retrieved by id, added params argument to our database function to prevent malicious SQL injections

---
+++ controllers/listings/show.php
@@ -0,0 +1,20 @@
+<?php
+
+//getting the config from the database
+$config = require basePath('config/db.php');
+//instantiate the database
+$db = new Database($config);
+
+$id = $_GET['id'] ?? '';
+// inspect($id);
+
+//add params array now to protect DB
+$params = [
+    'id' => $id
+];
+
+$listing = $db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+inspect($listing);
+
+loadView('listings/show');
