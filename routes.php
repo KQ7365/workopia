30d28872532dac3318c0fd772116a5fe3cf821f0
Kevin Quail <kevinq.7365@gmail.com>
2024-03-26 14:20:56 -0500

    refactored to allow route params. Slight issues with CSS file not wanted to load on an individual item

--- routes.php
+++ routes.php
@@ -6,6 +6,6 @@
 $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create');
-$router->get('/listing', 'ListingController@show');
+$router->get('/listing/{id}', 'ListingController@show');
 
 
